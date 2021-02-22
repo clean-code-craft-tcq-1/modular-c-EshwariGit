9dadc2937c79bda5d1e81ff95a121468d443a057
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-02-22 22:48:42 +0530

    Adding Spilt files for TestFunctions

---
+++ TestFunctions.h
@@ -0,0 +1,15 @@
+#ifndef TESTFUNCTIONS_H_
+#define TESTFUNCTIONS_H_
+
+
+#include <stdio.h>
+#include <assert.h>
+
+extern void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+    int expectedPairNumber);
+
+extern void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor);
