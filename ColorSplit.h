432e79bde16075cc7eae34bfbc36af4ecbd3d8a0
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-02-25 00:01:51 +0530

    Update ColorSplit.h

--- ColorSplit.h
+++ ColorSplit.h
@@ -5,6 +5,6 @@ typedef enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE, numberOfMinorColors_
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+    sizeof(MinorColor_type) / sizeof(MinorColor_type[0]);
 
 #endif
