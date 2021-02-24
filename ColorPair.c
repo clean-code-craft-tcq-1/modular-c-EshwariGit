82aac85fb70b93fd0d8d330b524d7fe3b2080c53
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-02-24 23:52:16 +0530

    Update ColorPair.c

--- ColorPair.c
+++ ColorPair.c
@@ -1,22 +1,20 @@
-#include "ColorPair.h"
+#include "ColorSplit.h"
 
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",
-        MajorColorNames[colorPair->majorColor],
-        MinorColorNames[colorPair->minorColor]);
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+const char * MinorColorToString(MinorColor_type minorcolor)
+{
+    return (MinorColorNames[minorcolor]);
 }
 
-ColorPair GetColorFromPairNumber(int pairNumber) {
-    ColorPair colorPair;
-    int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = 
-        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor =
-        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-    return colorPair;
-}
-
-int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
+const char * MajorColorToString(MajorColor_type majorcolor )
+{
+    return MajorColorNames[majorcolor];
 }
