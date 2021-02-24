317033102dccdad34b8cd4290e1a4ee02ab19d26
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-02-25 00:13:24 +0530

    Update ColorPair.c

--- ColorPair.c
+++ ColorPair.c
@@ -3,21 +3,21 @@
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
-        MajorColorNames[colorPair->majorColor],
-        MinorColorNames[colorPair->minorColor]);
+        MajorColorToString[colorPair->majorColor],
+        MinorColorToString[colorPair->minorColor]);
 }
 
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
     colorPair.majorColor = 
-        (MajorColor_type)(zeroBasedPairNumber / numberOfMinorColors);
+        (MajorColor_type)(zeroBasedPairNumber / numberOfMinorColorstype);
     colorPair.minorColor =
-        (MinorColor_type)(zeroBasedPairNumber % numberOfMinorColors);
+        (MinorColor_type)(zeroBasedPairNumber % numberOfMinorColorstype);
     return colorPair;
 }
 
 int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
+    return colorPair->majorColor * numberOfMinorColorstype +
             colorPair->minorColor + 1;
 }
