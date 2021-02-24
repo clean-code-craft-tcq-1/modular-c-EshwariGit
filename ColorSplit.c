122bda3b9bf69d05946e8749fa6a370154bdd8c4
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-02-25 00:14:17 +0530

    Update ColorSplit.c

--- ColorSplit.c
+++ ColorSplit.c
@@ -3,8 +3,7 @@
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
-int numberOfMajorColors =
-    sizeof(MajorColor_type) / sizeof(MajorColor_type[0]);
+
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
