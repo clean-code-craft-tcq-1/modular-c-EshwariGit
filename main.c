158e9ce3039fc82edc91176bd53681ba112443dc
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-02-24 23:55:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#include <assert.h>
+
 #include "TestFunctions.h"
+
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
+
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
+
     return 0;
 }
