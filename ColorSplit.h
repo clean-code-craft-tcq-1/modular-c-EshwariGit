2674d0ea7076de18dc139bc507ec288699e13638
EshwariGit <77387810+EshwariGit@users.noreply.github.com>
2021-02-25 00:29:07 +0530

    Update ColorSplit.h

--- ColorSplit.h
+++ ColorSplit.h
@@ -3,7 +3,7 @@
 typedef enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET, numberOfMajorColorstype} MajorColor_type;
 typedef enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE, numberOfMinorColorstype} MinorColor_type;
 
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+#define MAX_COLORPAIR_NAME_CHARS ((int)16);
 extern const char * MinorColorToString(MinorColor_type minorcolor);
 
 extern const char * MajorColorToString(MajorColor_type majorcolor );
